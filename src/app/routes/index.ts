0e145ccbaa78a6938e8eca331e9eb349e7aa5193
ArifulIslam Shuvo <arifulislams063@gmail.com>
2023-09-02 21:29:56 +0600

    Creating Academic Semesters- Implementing Functionality and Operations

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,13 +1,14 @@
 import express from 'express';
+import { AcademicSemesterRouter } from '../middlewares/modules/acdemicSemester/academicSemester.routes';
 
 const router = express.Router();
 
 const moduleRoutes = [
   // ... routes
   {
-    path: "",
-    routes: ""
-  }
+    path: '/academic-semesters',
+    route: AcademicSemesterRouter,
+  },
 ];
 
 moduleRoutes.forEach(route => router.use(route.path, route.route));
