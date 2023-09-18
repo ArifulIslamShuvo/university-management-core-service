2eb130bd9998eafa50fcdb76fa5ef3396db488ec
ArifulIslam Shuvo <arifulislams063@gmail.com>
2023-09-18 10:27:16 +0600

    Retrieving Student Data with Integration of Pagination Techniques

--- src/app/modules/student/student.routes.ts
+++ src/app/modules/student/student.routes.ts
@@ -4,6 +4,7 @@ import { StudentController } from './student.controller';
 import { StudentValidation } from './student.validations';
 
 const router = express.Router();
+router.get('/', StudentController.getAllFromDB);
 
 router.post(
   '/',
