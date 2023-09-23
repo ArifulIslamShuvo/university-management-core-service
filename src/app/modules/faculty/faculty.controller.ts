5d839e52d7603d55d4b8b6fd990bc9e0108235a1
ArifulIslam Shuvo <arifulislams063@gmail.com>
2023-09-23 19:25:04 +0600

    get all faculty

--- src/app/modules/faculty/faculty.controller.ts
+++ src/app/modules/faculty/faculty.controller.ts
@@ -1,7 +1,9 @@
 import { Request, Response } from 'express';
 import httpStatus from 'http-status';
 import catchAsync from '../../../shared/catchAsync';
+import pick from '../../../shared/pick';
 import sendResponse from '../../../shared/sendResponse';
+import { facultyFilterableFields } from './faculty.constants';
 import { FacultyService } from './faculty.service';
 
 const insertIntoDB = catchAsync(async (req: Request, res: Response) => {
@@ -15,7 +17,19 @@ const insertIntoDB = catchAsync(async (req: Request, res: Response) => {
     data: result,
   });
 });
-
+const getAllFromDB = catchAsync(async (req: Request, res: Response) => {
+  const filters = pick(req.query, facultyFilterableFields);
+  const options = pick(req.query, ['limit', 'page', 'sortBy', 'sortOrder']);
+  const result = await FacultyService.getAllFromDB(filters, options);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Faculties fetched successfully',
+    meta: result.meta,
+    data: result.data,
+  });
+});
 export const FacultyController = {
   insertIntoDB,
+  getAllFromDB,
 };
--- src/app/modules/faculty/faculty.routes.ts
+++ src/app/modules/faculty/faculty.routes.ts
@@ -4,7 +4,7 @@ import { FacultyController } from './faculty.controller';
 import { FacultyValidation } from './faculty.validations';
 
 const router = express.Router();
-
+router.get('/', FacultyController.getAllFromDB);
 router.post(
   '/',
   validateRequest(FacultyValidation.create),
