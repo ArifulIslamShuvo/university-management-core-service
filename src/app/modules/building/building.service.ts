c1f66288324480a9376e4299be029678443f5842
ArifulIslam Shuvo <arifulislams063@gmail.com>
2023-09-27 05:59:36 +0600

    Designing Building and Room Models, and Creating a New Building

---
+++ src/app/modules/building/building.service.ts
@@ -0,0 +1,12 @@
+import { Building } from '@prisma/client';
+import prisma from '../../../shared/prisma';
+
+const insertIntoDB = async (data: Building): Promise<Building> => {
+  const result = await prisma.building.create({
+    data,
+  });
+  return result;
+};
+export const BuildingService = {
+  insertIntoDB,
+};
--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { academicDepartmentRoutes } from '../modules/academicDepartment/academicDepartment.routes';
 import { academicFacultyRoutes } from '../modules/academicFaculty/academicFaculty.routes';
 import { AcademicSemesterRouter } from '../modules/acdemicSemester/academicSemester.routes';
+import { buildingRoutes } from '../modules/building/building.routes';
 import { facultyRoutes } from '../modules/faculty/faculty.routes';
 import { studentRoutes } from '../modules/student/student.routes';
 
@@ -28,6 +29,10 @@ const moduleRoutes = [
   {
     path: '/faculty',
     route: facultyRoutes,
+  },
+  {
+    path: '/buildings',
+    route: buildingRoutes,
   },
 ];
 
