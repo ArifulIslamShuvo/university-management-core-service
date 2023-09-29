7e90a8e4c8841a9aa3e5ff6117f11b368915eb30
ArifulIslam Shuvo <arifulislams063@gmail.com>
2023-09-29 09:03:41 +0600

    update and delete building data by id

--- src/app/modules/building/building.routes.ts
+++ src/app/modules/building/building.routes.ts
@@ -5,6 +5,13 @@ import { BuildingValidations } from './building.validations';
 const router = express.Router();
 router.get('/', BuildingController.getAllFromDB);
 router.get('/:id', BuildingController.getByIdFromDB);
+router.patch(
+  '/:id',
+  validateRequest(BuildingValidations.update),
+  BuildingController.updateOneInDB
+);
+
+router.delete('/:id', BuildingController.deleteByIdFromDB);
 
 router.post(
   '/',
